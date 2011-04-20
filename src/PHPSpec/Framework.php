e01a4fdba0760f2ddaafa3dddf6e6a3a7bafe0a4
Marcello Duarte <marcello.duarte@gmail.com>
2011-04-20 04:02:01 +0100

    Moved functions to a separate file

--- src/PHPSpec/Framework.php
+++ src/PHPSpec/Framework.php
@@ -10,25 +10,39 @@
  * http://www.gnu.org/licenses/lgpl-3.0.txt
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
- * to license@phpspec.org so we can send you a copy immediately.
+ * to license@phpspec.net so we can send you a copy immediately.
  *
  * @category   PHPSpec
  * @package    PHPSpec
- * @copyright  Copyright (c) 2007 P�draic Brady, Travis Swicegood
+ * @copyright  Copyright (c) 2007-2009 Pádraic Brady, Travis Swicegood
+ * @copyright  Copyright (c) 2010-2011 Pádraic Brady, Travis Swicegood, Marcello Duarte
  * @license    http://www.gnu.org/licenses/lgpl-3.0.txt GNU Lesser General Public Licence Version 3
  */
 namespace PHPSpec;
 
+/**
+ * @see \PHPSpec\Exception.php
+ */
 require_once 'PHPSpec/Exception.php';
+
+require_once 'PHPSpec/Describe/Functions.php';
 
 /**
  * @category   PHPSpec
  * @package    PHPSpec
- * @copyright  Copyright (c) 2007 P�draic Brady, Travis Swicegood
+ * @copyright  Copyright (c) 2007-2009 Pádraic Brady, Travis Swicegood
+ * @copyright  Copyright (c) 2010-2011 Pádraic Brady, Travis Swicegood, Marcello Duarte
  * @license    http://www.gnu.org/licenses/lgpl-3.0.txt GNU Lesser General Public Licence Version 3
  */
 class Framework
 {
+    /**
+     * Framework loader
+     * 
+     * @param string $class
+     * @throws \PHPSpec\Exception
+     * @return boolean
+     */
     public static function autoload($class)
     {
         // @todo consider speed implications
@@ -52,23 +66,8 @@ spl_autoload_register(array(
     'autoload'
 ));
 
-function describe()
-{
-    $args = func_get_args();
-    return call_user_func_array(array("\\PHPSpec\\Specification",'getSpec'), $args);
+if (!defined('PHPSPEC_COMMAND_CALL')) {
+	$command = new \PHPSpec\Console\Command;
+	$command->run();
 }
 
-function PHPSpec_ErrorHandler($errno, $errstr, $errfile, $errline)
-{
-    if (!($errno & error_reporting())) {
-        return;
-    }
-
-    $backtrace = debug_backtrace();
-    array_shift($backtrace);
-
-    include_once 'PHPSpec/Runner/ErrorException.php';
-    throw new \PHPSpec\Runner\ErrorException($errstr, $errno, $errfile, $errline, $backtrace);
-
-    return true;
-}
