9cb9fcb6e642228c67f97922e344b0a7c2291f93
Marcello Duarte <marcello.duarte@gmail.com>
2011-04-22 19:00:07 +0100

    More API documentation

--- src/PHPSpec/Console/Getopt.php
+++ src/PHPSpec/Console/Getopt.php
@@ -10,30 +10,45 @@
  * http://www.gnu.org/licenses/lgpl-3.0.txt
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
- * to license@phpspec.org so we can send you a copy immediately.
+ * to license@phpspec.net so we can send you a copy immediately.
  *
- * @category   PHPSpec
- * @package    PHPSpec
- * @copyright  Copyright (c) 2007 P�draic Brady, Travis Swicegood
- * @license    http://www.gnu.org/licenses/lgpl-3.0.txt GNU Lesser General Public Licence Version 3
+ * @category  PHPSpec
+ * @package   PHPSpec
+ * @copyright Copyright (c) 2007-2009 Pádraic Brady, Travis Swicegood
+ * @copyright Copyright (c) 2010-2011 Pádraic Brady, Travis Swicegood,
+ *                                    Marcello Duarte
+ * @license   http://www.gnu.org/licenses/lgpl-3.0.txt GNU Lesser General Public Licence Version 3
  */
 namespace PHPSpec\Console;
 
 /**
  * @category   PHPSpec
  * @package    PHPSpec
- * @copyright  Copyright (c) 2007 P�draic Brady, Travis Swicegood
+ * @copyright  Copyright (c) 2007-2009 Pádraic Brady, Travis Swicegood
+ * @copyright  Copyright (c) 2010-2011 Pádraic Brady, Travis Swicegood,
+ *                                     Marcello Duarte
  * @license    http://www.gnu.org/licenses/lgpl-3.0.txt GNU Lesser General Public Licence Version 3
  */
 class Getopt
 {
+    /**
+     * @var array
+     */
     protected $_options = array();
 
+    /**
+     * Constructs the object with the argument variables. If none is given it
+     * gets them from <code>$_SERVER['argv']</code>. Parses all arguments
+     * into the options property
+     * 
+     * @param array $argv
+     */
     public function __construct(array $argv = null)
     {
         if (is_null($argv)) {
             $argv = $_SERVER['argv'];
         }  
+        // FIXME! Work in constructor 
         $this->_parse($argv);
 
         if (!isset($this->_options['reporter'])) {
@@ -41,44 +56,90 @@ class Getopt
         }
     }
 
+    /**
+     * Gets an option
+     * 
+     * @param string $name
+     * @return NULL|string
+     */
     public function getOption($name)
     {
         return isset($this->_options[$name]) ? $this->_options[$name] : null;
     }
 
+    /**
+     * Gets an option with object operator directly
+     * 
+     * @param string $name
+     * @return NULL|string
+     */
     public function __get($name)
     {
         return $this->getOption($name);
     }
 
+    /**
+     * Sets an option
+     * 
+     * @param string $name
+     * @param string $value
+     */
     public function setOption($name, $value)
     {
         $this->_options[$name] = $value;
     }
 
+    /**
+     * Sets an option with object operator directly
+     * 
+     * @param string $name
+     * @param string $value
+     */
     public function __set($name, $value)
     {
         $this->setOption($name, $value);
     }
 
+    /**
+     * Checks if an option has been set
+     * 
+     * @param string $name
+     */
     public function hasOption($name)
     {
         return isset($this->_options[$name]);
     }
 
-	public function noneGiven()
-	{
-		return $this->_options['noneGiven'] === true;
-	}
+    /**
+     * Checks if noneGiven flag has been set to true. This flag is set to true
+     * in case there is nothing in argv, to stop the parser from doing any
+     * unecessary work
+     * 
+     * @return boolean
+     */
+    public function noneGiven()
+    {
+        return $this->_options['noneGiven'] === true;
+    }
 
+    /**
+     * Checks if an option has been set calling <code>isset</code> on the
+     * options array
+     * 
+     * @param string $name
+     */
     public function __isset($name)
     {
         return $this->hasOption($name);
     }
 
+    /**
+     * Enter description here ...
+     * @param array $argv
+     */
     protected function _parse(array $argv)
     { 
-	  
+      
         // get rid of the Command.php reference
         if (is_file($argv[0])) {
             array_shift($argv);
@@ -90,7 +151,8 @@ class Getopt
             return;
         }
                                         
-        // if the first argument is not a - or -- option it should be a spec filename
+        // if the first argument is not a - or -- option
+        // it should be a spec filename
         if ($argv[0]{0} !== '-') {
             $this->_options['specFile'] = $argv[0];
             array_shift($argv);
@@ -126,8 +188,6 @@ class Getopt
                 $this->_options[$encountered] = $parts[1];
             }
         }
-
-        
     }
 
 }
--- src/PHPSpec/Context/Zend.php
+++ src/PHPSpec/Context/Zend.php
@@ -10,18 +10,25 @@
  * http://www.gnu.org/licenses/lgpl-3.0.txt
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
- * to license@phpspec.org so we can send you a copy immediately.
+ * to license@phpspec.net so we can send you a copy immediately.
  *
- * @category   PHPSpec
- * @package    PHPSpec
- * @copyright  Copyright (c) 2007 Pádraic Brady, Travis Swicegood
- * @license    http://www.gnu.org/licenses/lgpl-3.0.txt GNU Lesser General Public Licence Version 3
+ * @category  PHPSpec
+ * @package   PHPSpec
+ * @copyright Copyright (c) 2007-2009 Pádraic Brady, Travis Swicegood
+ * @copyright Copyright (c) 2010-2011 Pádraic Brady, Travis Swicegood,
+ *                                    Marcello Duarte
+ * @license   http://www.gnu.org/licenses/lgpl-3.0.txt GNU Lesser General Public Licence Version 3
  */
-
 namespace PHPSpec\Context;
 
+/**
+ * @see \Zend\Controller\Front
+ */
 require_once 'Zend/Controller/Front.php';
 
+/**
+ * @see \Zend\Controller\Request\Http
+ */
 require_once 'Zend/Controller/Request/Http.php';
 
 // require_once 'Zend/Di.php';
@@ -31,18 +38,38 @@ use \PHPSpec\Context;
 /**
  * @category   PHPSpec
  * @package    PHPSpec
- * @copyright  Copyright (c) 2007 Pádraic Brady, Travis Swicegood
+ * @copyright  Copyright (c) 2007-2009 Pádraic Brady, Travis Swicegood
+ * @copyright  Copyright (c) 2010-2011 Pádraic Brady, Travis Swicegood,
+ *                                     Marcello Duarte
  * @license    http://www.gnu.org/licenses/lgpl-3.0.txt GNU Lesser General Public Licence Version 3
  */
 class Zend extends Context
 {
 
+    /**
+     * @FIXME! This is probably not meant to be a constant?
+     */
     const BASE_URL = 'http://www.example.com';
 
+    /**
+     * Directories where the modules are
+     * 
+     * @var array
+     */
     protected static $_moduleDirectories = array();
 
+    /**
+     * Directories where the controllers are
+     * 
+     * @var array
+     */
     protected static $_controllerDirectories = array();
 
+    /**
+     * Enter description here ...
+     * 
+     * @var unknown_type
+     */
     protected static $_frontControllerSetupCallback = null;
 
     /**
@@ -74,6 +101,9 @@ class Zend extends Context
      */
     protected $_response = null;
 
+    /**
+     * Enter description here ...
+     */
     public function __construct()
     {
         parent::__construct();
@@ -81,31 +111,54 @@ class Zend extends Context
         $this->_frontController = \Zend_Controller_Front::getInstance();
     }
 
+    /**
+     * Enter description here ...
+     * @param unknown_type $callback
+     */
     public static function setFrontControllerSetupCallback($callback)
     {
         self::$_frontControllerSetupCallback = $callback;
     }
 
+    /**
+     * Enter description here ...
+     */
     public static function clearFrontControllerSetupCallback()
     {
         self::$_frontControllerSetupCallback = null;
     }
 
+    /**
+     * Enter description here ...
+     * @param unknown_type $path
+     */
     public static function addModuleDirectory($path)
     {
         self::$_moduleDirectories[] = $path;
     }
 
+    /**
+     * Enter description here ...
+     * @return unknown_type
+     */
     public static function getModuleDirectories()
     {
         return self::$_moduleDirectories;
     }
 
+    /**
+     * Enter description here ...
+     */
     public static function clearModuleDirectories()
     {
         self::$_moduleDirectories = array();
     }
 
+    /**
+     * Enter description here ...
+     * @param unknown_type $path
+     * @param unknown_type $module
+     */
     public static function addControllerDirectory($path, $module = null)
     {
         if (!isset($module)) {
@@ -115,16 +168,26 @@ class Zend extends Context
         self::$_controllerDirectories[$module] = $path;
     }
 
+    /**
+     * Enter description here ...
+     * @return unknown_type
+     */
     public static function getControllerDirectories()
     {
         return self::$_controllerDirectories;
     }
 
+    /**
+     * Enter description here ...
+     */
     public static function clearControllerDirectories()
     {
         self::$_controllerDirectories = array();
     }
 
+    /**
+     * Enter description here ...
+     */
     public function beforeEach()
     {
         $_GET = array();
@@ -133,6 +196,13 @@ class Zend extends Context
         $this->_clearFrontController();
     }
 
+    /**
+     * Enter description here ...
+     * @param unknown_type $actionName
+     * @param array $getArray
+     * @param array $paramArray
+     * @return Zend_Controller_Response_Http
+     */
     public function get($actionName, array $getArray = null, array $paramArray = null)
     {
         if (!empty($getArray)) {
@@ -142,6 +212,13 @@ class Zend extends Context
         return $this->response();
     }
 
+    /**
+     * Enter description here ...
+     * @param unknown_type $actionName
+     * @param array $postArray
+     * @param array $paramArray
+     * @return Zend_Controller_Response_Http
+     */
     public function post($actionName, array $postArray = null, array $paramArray = null)
     {
         if (!empty($postArray)) {
@@ -161,6 +238,11 @@ class Zend extends Context
         return $this->_request;
     }
 
+    /**
+     * Enter description here ...
+     * @throws \PHPSpec\Exception
+     * @return Zend_Controller_Response_Http
+     */
     public function response()
     {
         if (!isset($this->_response)) {
@@ -170,6 +252,9 @@ class Zend extends Context
         return $this->_response;
     }
     
+    /**
+     * Enter description here ...
+     */
     public function replaceClass()
     {
         $args = func_get_args();
@@ -177,21 +262,39 @@ class Zend extends Context
         $method->invokeArgs(null, $args);
     }
 
+    /**
+     * Enter description here ...
+     * @param unknown_type $controllerName
+     */
     public function setController($controllerName)
     {
         $this->_controller = $controllerName;
     }
 
+    /**
+     * Enter description here ...
+     * @return string
+     */
     public function getController()
     {
         return $this->_controller;
     }
 
+    /**
+     * Enter description here ...
+     * @return Ambiguous
+     */
     public function getFrontController()
     {
         return $this->_frontController;
     }
 
+    /**
+     * Enter description here ...
+     * @param unknown_type $actionName
+     * @param array $paramArray
+     * @return Ambiguous
+     */
     protected function _makeRequest($actionName, array $paramArray = null)
     {
         if (preg_match("%/%", $actionName)) {
@@ -212,15 +315,45 @@ class Zend extends Context
         $response->setContext($this);
         return $response;
     }
+    
+    /**
+     * Returns the callback size, 2 for array callback, 1 for strings or
+     * anonymous functions and 0 for no callback.
+     * 
+     * @return integer
+     */
+    private function callbackSize()
+    {
+    	if (is_array(self::$_frontControllerSetupCallback)) {
+    		return count(self::$_frontControllerSetupCallback);
+    	}
+    	if ((is_string(self::$_frontControllerSetupCallback) &&
+    	    trim(self::$_frontControllerSetupCallback) !== '') ||
+    	    is_callable(self::$_frontControllerSetupCallback)) {
+    		return 1;
+    	}
+    	return 0;
+    }
 
+    /**
+     * Runs the front controller setup callback. If none is set runs the default
+     * which sets <code>returnResponse</code>, <code>throwExceptions</code>,
+     * <code>controllerDirectories</code> and <code>modulesDirectory</code>
+     */
     protected function _clearFrontController() {
         $this->_frontController->resetInstance();
-        if (count(self::$_frontControllerSetupCallback) > 0) {
-            call_user_func(array(
-                self::$_frontControllerSetupCallback[0],
-                self::$_frontControllerSetupCallback[1]
-            ));
-        } else {
+ 
+        switch ($this->callbackSize()) {
+        case 2:
+        	 call_user_func(array(
+	            self::$_frontControllerSetupCallback[0],
+	            self::$_frontControllerSetupCallback[1]
+	         ));
+	         break;
+	    case 1:
+        	 call_user_func(self::$_frontControllerSetupCallback);
+	         break;
+        case 0:
             $this->_frontController->returnResponse(true);
             $this->_frontController->throwExceptions(true);
             foreach (self::getControllerDirectories() as $module=>$path) {
@@ -232,14 +365,32 @@ class Zend extends Context
             foreach (self::getModuleDirectories() as $path) {
                 $this->_frontController->addModuleDirectory($path);
             }
+            break;
+        default:
+        	throw new \RuntimeException(
+        	    'Invalid callback for front controller setup'
+        	);
         }
 
     }
 
+    /**
+     * Sets the controller based on the context name
+     */
     protected function _setController()
     {
-        $controllerClassName = substr(get_class($this), 8);
-        $this->_controller = substr($controllerClassName, 0, strlen($controllerClassName)-10);
+    	if (strpos(strtolower(get_class($this)), 'describe') === 0) {
+    		$controllerClassName = substr(get_class($this), 8);
+
+    	} else { // ends with spec
+    		$controllerClassName = substr(
+    		    get_class($this), 0, strlen(get_class($this)) - 4
+    		);
+    	}
+    	
+        $this->_controller = substr(
+            $controllerClassName, 0, strlen($controllerClassName)-10
+        );
     }
 
 }
