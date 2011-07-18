175012efb114c3efec431dee5357ff35480c1f19
Marcello Duarte <marcello.duarte@gmail.com>
2011-07-18 02:35:24 +0100

    Added zend filter and validate to enhace support for zend view context

--- src/PHPSpec/Specification/Example.php
+++ src/PHPSpec/Specification/Example.php
@@ -26,7 +26,8 @@ use \PHPSpec\Runner\Reporter,
     \PHPSpec\Specification\Result\Error,
     \PHPSpec\Specification\Result\Pending,
     \PHPSpec\Specification\Result\DeliberateFailure,
-    \PHPSpec\Specification\Result\Failure;
+    \PHPSpec\Specification\Result\Failure,
+    \PHPSpec\Util\Filter;
 
 /**
  * @category   PHPSpec
@@ -123,10 +124,6 @@ class Example
     public function getSpecificationText()
     {
         $methodName = substr($this->_example->getName(), 2);
-        $terms = preg_split(
-            "/(?=[[:upper:]])/", $methodName, -1, PREG_SPLIT_NO_EMPTY
-        );
-        $termsLowercase = array_map('strtolower', $terms);
-        return implode(' ', $termsLowercase);
+        return Filter::camelCaseToSpace($methodName);
     }
 }
---
+++ src/PHPSpec/Util/Filter.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * PHPSpec
+ *
+ * LICENSE
+ *
+ * This file is subject to the GNU Lesser General Public License Version 3
+ * that is bundled with this package in the file LICENSE.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@phpspec.net so we can send you a copy immediately.
+ *
+ * @category  PHPSpec
+ * @package   PHPSpec
+ * @copyright Copyright (c) 2007-2009 Pádraic Brady, Travis Swicegood
+ * @copyright Copyright (c) 2010-2011 Pádraic Brady, Travis Swicegood,
+ *                                    Marcello Duarte
+ * @license   http://www.gnu.org/licenses/lgpl-3.0.txt GNU Lesser General Public Licence Version 3
+ */
+namespace PHPSpec\Util;
+
+/**
+ * @category   PHPSpec
+ * @package    PHPSpec
+ * @copyright  Copyright (c) 2007-2009 Pádraic Brady, Travis Swicegood
+ * @copyright  Copyright (c) 2010-2011 Pádraic Brady, Travis Swicegood,
+ *                                     Marcello Duarte
+ * @license    http://www.gnu.org/licenses/lgpl-3.0.txt GNU Lesser General Public Licence Version 3
+ */
+class Filter
+{
+    /**
+     * Converst camel case to dash
+     *
+     * @param string $pattern
+     * @return string
+     */
+    public static function camelCaseToDash($pattern)
+    {
+        return self::camelCaseToSeparator($pattern, '-');
+    }
+    
+    /**
+     * Converst camel case to space
+     *
+     * @param string $pattern
+     * @return string
+     */
+    public static function camelCaseToSpace($pattern)
+    {
+        return self::camelCaseToSeparator($pattern, ' ');
+    }
+    
+    /**
+     * Converst camel case to separator
+     *
+     * @param string $pattern
+     * @return string
+     */
+    public static function camelCaseToSeparator($pattern, $sep)
+    {
+        $terms = preg_split(
+            "/(?=[[:upper:]])/", $pattern, -1, PREG_SPLIT_NO_EMPTY
+        );
+        $termsLowerCase = array_map('strtolower', $terms);
+        return implode($sep, $termsLowerCase);
+    }
+}
