fabb7acb7c317ff8bb3419cbc70edd2c09eb7365
Marcello Duarte <marcello.duarte@gmail.com>
2011-08-27 16:42:32 +0100

    Spec: Runner will set the formatter to display colours

--- spec/Runner/Cli/RunnerSpec.php
+++ spec/Runner/Cli/RunnerSpec.php
@@ -3,23 +3,17 @@
 namespace Spec\PHPSpec\Runner\Cli;
 
 require_once __DIR__ . '/../../SpecHelper.php';
+require_once '/md/dev/php/phpspec/src/PHPSpec/Runner/Reporter.php';
+require_once '/md/dev/php/phpspec/src/PHPSpec/Runner/Cli/Reporter.php';
 
 use \PHPSpec\Runner\Cli\Runner as CliRunner;
 
 class DescribeRunner extends \PHPSpec\Context
 {
-    function before()
-    {
-        $this->runner = $this->spec(new CliRunner);
-    }
-    
     function itWillHaltTheRunAndSetVersionMessageIfVersionOptionIsSet()
     {
-        $reporter = $this->mock('\PHPSpec\Runner\Reporter');
-        $reporter->shouldReceive('setMessage')->with(CliRunner::VERSION);
-        
-        $world = $this->mock('\PHPSpec\World');
-        $world->shouldReceive('getOption')->with('version')->andReturn(true);
+        $reporter = $this->getReporterStubWithMessage(CliRunner::VERSION);
+        $world = $this->getWorldStub(self::SHOW_VERSION);
         $world->shouldReceive('getReporter')->andReturn($reporter);
 
         $this->runner->run($world)->should->beNull();
@@ -27,14 +21,77 @@ class DescribeRunner extends \PHPSpec\Context
     
     function itWillHaltTheRunAndSetHelpMessageIfHelpOptionIsSet()
     {
-        $reporter = $this->mock('\PHPSpec\Runner\Reporter');
-        $reporter->shouldReceive('setMessage')->with(CliRunner::USAGE);
-        
-        $world = $this->mock('\PHPSpec\World');
-        $world->shouldReceive('getOption')->with('version')->andReturn(false);
-        $world->shouldReceive('getOption')->with('h')->andReturn(true);
+        $reporter = $this->getReporterStubWithMessage(CliRunner::USAGE);
+        $world = $this->getWorldStub(self::DONT_SHOW_VERSION, self::SHOW_HELP);
         $world->shouldReceive('getReporter')->andReturn($reporter);
 
         $this->runner->run($world)->should->beNull();
     }
+    
+    function itWillSetTheFormatterToDisplayColours()
+    {
+        $formatter = $this->mock('\PHPSpec\Runner\Formatter');
+        $reporter = $this->mock('\PHPSpec\Runner\Cli\Reporter[getFormatter,attach,setRuntimeStart]');
+        $reporter->shouldReceive('getFormatter')->andReturn($formatter);
+        $reporter->shouldReceive('attach')->with($formatter);
+        $reporter->shouldReceive('setRuntimeStart');
+        $world = $this->mock('\PHPSpec\World[getReporter,getOption]');
+        $world->shouldReceive('getReporter')->andReturn($reporter);        
+        
+        $this->setOptionsAsFalse($world, array('version', 'h', 'help', 'b', 'failfast', 'example', 'specFile'));
+        $this->setOptionsAsTrue($world, array('c'));
+        
+        $world->shouldReceive('getOption')->with('c')->andReturn(true);
+        
+        $formatter->shouldReceive('setShowColors')->with(true)->times(1);
+        
+        try {
+            $this->runner->run($world);
+        } catch (\PHPSpec\Runner\Cli\Error $e) {
+            // because I am setting no spec file
+        }
+    }
+    
+    function before()
+    {
+        $this->runner = $this->spec(new CliRunner);
+    }
+    
+    function setOptionsAsFalse($world, $options)
+    {
+        foreach($options as $option) {
+            $world->shouldReceive('getOption')->with($option)->andReturn(false);
+        }
+    }
+    
+    function setOptionsAsTrue($world, $options)
+    {
+        foreach($options as $option) {
+            $world->shouldReceive('getOption')->with($option)->andReturn(true);
+        }
+    }
+    
+    function getReporterStubWithMessage($message)
+    {
+        $reporter = $this->mock('\PHPSpec\Runner\Reporter');
+        $reporter->shouldReceive('setMessage')->with($message);
+        return $reporter;
+    }
+    
+    function getWorldStub($version = false, $help = false)
+    {
+        $world = $this->mock('\PHPSpec\World');
+        $this->setVersionAndHelp($world, $version, $help);
+        return $world;
+    }
+    
+    function setVersionAndHelp($world, $version, $help)
+    {
+        $world->shouldReceive('getOption')->with('version')->andReturn($version);
+        $world->shouldReceive('getOption')->with('h')->andReturn($help);
+        $world->shouldReceive('getOption')->with('help')->andReturn($help);
+    }
+    
+    const SHOW_HELP = true, SHOW_VERSION = true, DONT_SHOW_HELP = false,
+          DONT_SHOW_VERSION = false;
 }
