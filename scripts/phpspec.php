8a50bd375c3376750f7438fda7c521d042a865a8
Luis Cordova <cordoval@gmail.com>
2012-01-05 23:20:39 -0500

    added Matcher containtText and spec added matcher on interceptor hard coded class taking out some hunks added a stack trace line to disable/enable good for debugging added properties to null on containText matcher fixed matches on containText --- passing standard Zend phpcs --- passing

--- scripts/phpspec.php
+++ scripts/phpspec.php
@@ -171,6 +171,7 @@
  */
 ini_set('display_errors', 1);
 error_reporting(E_ALL|E_STRICT);
+ini_set('xdebug.show_exception_trace', 'Off');
 
 require_once 'PHPSpec/Loader/UniversalClassLoader.php';
 include_once 'Mockery.php';
