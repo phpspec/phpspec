a0f20fbe35e5374271d1f6242b2256df436c5a71
Amjad Mohamed <amjad@alliedinsure.net>
2012-03-12 08:51:22 +0500

    Added Text_Template as a composer dependency and also added Text_Template to be autoloaded in normal phpspec script

--- scripts/phpspec-composer.php
+++ scripts/phpspec-composer.php
@@ -7,7 +7,7 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL|E_STRICT);
 
 require_once 'vendor/.composer/autoload.php';
-set_include_path(__DIR__.'/../src' . PATH_SEPARATOR . get_include_path());
+set_include_path(__DIR__.'/../src');
 $phpspec = new \PHPSpec\PHPSpec($argv);
 $phpspec->execute();
 
--- scripts/phpspec.php
+++ scripts/phpspec.php
@@ -174,10 +174,16 @@ error_reporting(E_ALL|E_STRICT);
 
 require_once 'PHPSpec/Loader/UniversalClassLoader.php';
 include_once 'Mockery.php';
+
+$spec_source = __DIR__ . '/../src/';
+
 $paths = explode(':', ini_get('include_path'));
 $loader = new \PHPSpec\Loader\UniversalClassLoader();
-$loader->registerNamespace('PHPSpec', $paths);
+$loader->registerNamespace('PHPSpec', $spec_source);
 $loader->registerNamespace('Mockery', $paths);
+
+$loader->registerPrefix('Text_', $paths);
+
 $loader->register();
 
 $phpspec = new \PHPSpec\PHPSpec($argv);
