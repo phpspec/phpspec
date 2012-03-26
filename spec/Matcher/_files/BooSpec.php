7e177d4ca9030edf9df5e437f0cb97d532a558b4
Marcello Duarte <marcello.duarte@gmail.com>
2012-03-26 23:14:52 +0100

    Add a body to BooSpec so it doesn't complain about being empty

--- spec/Matcher/_files/BooSpec.php
+++ spec/Matcher/_files/BooSpec.php
@@ -4,8 +4,12 @@ if (!defined ('DESCRIBE_BOO')) {
 	define('DESCRIBE_BOO', 'describeBoo');
 	
 	class DescribeBoo extends \PHPSpec\Context {
-		public function itShouldBeTrue() {}
-		public function itShouldBeFalse() {}
+		public function itShouldBeTrue() {
+		    $x = 'just so that it is not empty';
+		}
+		public function itShouldBeFalse() {
+		    $x = 'just so that it is not empty';
+		}
 	}
 }
 
