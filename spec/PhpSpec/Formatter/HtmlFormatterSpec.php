66007abfd8b33d104669a5a1395134e6fcb39ff7
Marcello Duarte <marcello.duarte@gmail.com>
2013-06-10 20:01:18 +0100

    Add html pending example template

--- spec/PhpSpec/Formatter/HtmlFormatterSpec.php
+++ spec/PhpSpec/Formatter/HtmlFormatterSpec.php
@@ -10,15 +10,42 @@ use PhpSpec\Console\IO;
 
 class HtmlFormatterSpec extends ObjectBehavior
 {
-    const PASSED_TEMPLATE_OUTPUT = '          <dd class="example passed">it works</dd>';
+    const EVENT_TITLE = 'it works';
     
     function it_prints_the_passed_template_when_example_pass(IO $io, ExampleEvent $event)
     {
         $this->setIO($io);
         $event->getResult()->willReturn(ExampleEvent::PASSED);
-        $event->getTitle()->willReturn('it works');
-        $io->write(self::PASSED_TEMPLATE_OUTPUT)->shouldBeCalled();
+        $event->getTitle()->willReturn(self::EVENT_TITLE);
+
+        $io->write($this->passingTemplate(self::EVENT_TITLE))->shouldBeCalled();
         
         $this->afterExample($event);
+    }
+
+    function it_prints_the_pending_template_when_example_is_pending(IO $io, ExampleEvent $event)
+    {
+        $this->setIO($io);
+        $event->getResult()->willReturn(ExampleEvent::PENDING);
+        $event->getTitle()->willReturn(self::EVENT_TITLE);
+
+        $io->write($this->pendingTemplate(self::EVENT_TITLE, 1))->shouldBeCalled();
+        
+        $this->afterExample($event);
+    }
+
+    function passingTemplate($title)
+    {
+        return '          <dd class="example passed">' . $title . '</dd>';
+    }
+
+    function pendingTemplate($title, $pendingExamplesCount)
+    {
+        return '             <dd class="example not_implemented">
+      <span class="not_implemented_spec_name">' . $title . '</span>
+      <script type="text/javascript">makeYellow(\'phpspec-header\');</script>
+      <script type="text/javascript">makeYellow(\'div_group_' . $pendingExamplesCount . '\');</script>
+      <script type="text/javascript">makeYellow(\'example_group_' . $pendingExamplesCount . '\');</script>
+    </dd>';
     }
 }
--- src/PhpSpec/Formatter/HtmlFormatter.php
+++ src/PhpSpec/Formatter/HtmlFormatter.php
@@ -6,15 +6,36 @@ use PhpSpec\Event\ExampleEvent;
 
 class HtmlFormatter extends BasicFormatter
 {
-    const PASSED_TEMPLATE_OUTPUT = '          <dd class="example passed">%s</dd>';
     public function afterExample(ExampleEvent $event)
     {
         $io = $this->getIO();
 
         switch($event->getResult()) {
             case ExampleEvent::PASSED :
-                $io->write(sprintf(static::PASSED_TEMPLATE_OUTPUT, $event->getTitle()));
+                $io->write(($this->passedTemplateOutput($event->getTitle())));
+                break;
+            case ExampleEvent::PENDING :
+                static $pendingExamplesCount = 1;
+                $io->write(sprintf($this->pendingTemplateOutput(
+                    $event->getTitle(),
+                    $pendingExamplesCount++
+                )));
                 break;
         }
+    }
+
+    private function passedTemplateOutput($title)
+    {
+        return sprintf('          <dd class="example passed">%s</dd>', $title);
+    }
+
+    private function pendingTemplateOutput($title, $pendingExamplesCount)
+    {
+        return '             <dd class="example not_implemented">
+      <span class="not_implemented_spec_name">' . $title . '</span>
+      <script type="text/javascript">makeYellow(\'phpspec-header\');</script>
+      <script type="text/javascript">makeYellow(\'div_group_' . $pendingExamplesCount . '\');</script>
+      <script type="text/javascript">makeYellow(\'example_group_' . $pendingExamplesCount . '\');</script>
+    </dd>';
     }
 }
