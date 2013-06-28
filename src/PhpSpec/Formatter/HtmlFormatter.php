d50ef50abe3700bfe6dc498d8fb2fc7225cc328e
Marcello Duarte <marcello.duarte@gmail.com>
2013-06-28 08:10:26 +0100

    Add a header and a footer as suite events

--- src/PhpSpec/Formatter/HtmlFormatter.php
+++ src/PhpSpec/Formatter/HtmlFormatter.php
@@ -3,6 +3,7 @@
 namespace PhpSpec\Formatter;
 
 use PhpSpec\Event\ExampleEvent;
+use PhpSpec\Event\SuiteEvent;
 use PhpSpec\Formatter\Presenter\PresenterInterface;
 
 class HtmlFormatter extends BasicFormatter
@@ -20,9 +21,21 @@ class HtmlFormatter extends BasicFormatter
         $this->presenter = $presenter;
     }
 
+    public function beforeSuite(SuiteEvent $suite)
+    {
+        include __DIR__ . "/Html/ReportHeader.html";
+    }
+
     public function afterExample(ExampleEvent $event)
     {
         $reportLine = $this->reportItemFactory->create($this->getIO(), $event, $this->presenter);
         $reportLine->write();
+        $this->getIO()->write(PHP_EOL);
+    }
+
+    public function afterSuite(SuiteEvent $suite)
+    {
+        include __DIR__ . "/Html/ReportSummary.html";
+        include __DIR__ . "/Html/ReportFooter.html";
     }
 }
