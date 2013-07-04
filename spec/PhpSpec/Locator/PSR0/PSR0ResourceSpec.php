011bdb3bf687f0f2b04ea5a96cfd6ec97973a6a0
Christophe Coevoet <stof@notk.org>
2013-07-04 18:52:40 +0200

    Fixed the expected directory separators when running the specs on Windows

--- spec/PhpSpec/Locator/PSR0/PSR0ResourceSpec.php
+++ spec/PhpSpec/Locator/PSR0/PSR0ResourceSpec.php
@@ -32,7 +32,7 @@ class PSR0ResourceSpec extends ObjectBehavior
     {
         $locator->getFullSrcPath()->willReturn('/local/');
 
-        $this->getSrcFilename()->shouldReturn('/local/usr/lib/config.php');
+        $this->getSrcFilename()->shouldReturn('/local/usr'.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'config.php');
     }
 
     function it_generates_src_namespace_from_provided_parts_using_locator($locator)
@@ -68,7 +68,7 @@ class PSR0ResourceSpec extends ObjectBehavior
     {
         $locator->getFullSpecPath()->willReturn('/local/spec/');
 
-        $this->getSpecFilename()->shouldReturn('/local/spec/usr/lib/configSpec.php');
+        $this->getSpecFilename()->shouldReturn('/local/spec/usr'.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'configSpec.php');
     }
 
     function it_generates_spec_namespace_from_provided_parts_using_locator($locator)
