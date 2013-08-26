5634b6c7b9f0200ab4008690147a4d05041450ca
Ciaran McNulty <cmcnulty@inviqa.com>
2013-08-26 21:09:53 +0100

    Confirmation dialogs now respect --no-interaction option
    Response to issue #143
    
    This is actually fixed upstream in Symfony console
    [https://github.com/symfony/symfony/issues/8079] but this fixes it
    until that makes it into a public release.

--- src/PhpSpec/Console/IO.php
+++ src/PhpSpec/Console/IO.php
@@ -122,6 +122,10 @@ class IO
 
     public function askConfirmation($question, $default = true)
     {
+        if (!$this->input->isInteractive()) {
+            return $default;
+        }
+
         $lines   = array();
         $lines[] = '<question>'.str_repeat(' ', 70)."</question>";
         foreach (explode("\n", wordwrap($question), 50) as $line) {
