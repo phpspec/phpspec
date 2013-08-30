e71a8a485f88367bf49890d5ee67658fa48f276b
Marcello Duarte <marcello.duarte@gmail.com>
2013-08-30 19:29:10 +0100

    Add an instantiator to create objects without constructor

---
+++ spec/PhpSpec/Util/InstantiatorSpec.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace spec\PhpSpec\Util;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class InstantiatorSpec extends ObjectBehavior
+{
+    function it_creates_an_instance()
+    {
+        $this->instantiate('spec\PhpSpec\Util\NoConstructor')
+            ->shouldBeAnInstanceOf('spec\PhpSpec\Util\NoConstructor');
+    }
+
+    function it_creates_an_instance_ignoring_constructor()
+    {
+        $this->instantiate('spec\PhpSpec\Util\WithConstructor')
+            ->shouldBeAnInstanceOf('spec\PhpSpec\Util\WithConstructor');
+    }
+    
+}
+
+class NoConstructor
+{
+    
+}
+
+class WithConstructor
+{
+    public function __construct($requiredArgument)
+    {
+    }
+}
