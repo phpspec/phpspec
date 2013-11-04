64a5cb5ab6dad8d0d18fcdaa6757e3dd8c374016
Marcello Duarte <marcello.duarte@gmail.com>
2013-11-04 19:22:45 +0000

    Add missing specs for constructor decorator

--- spec/PhpSpec/Wrapper/Subject/Expectation/ConstructorDecoratorSpec.php
+++ spec/PhpSpec/Wrapper/Subject/Expectation/ConstructorDecoratorSpec.php
@@ -3,6 +3,8 @@
 namespace spec\PhpSpec\Wrapper\Subject\Expectation;
 
 use PhpSpec\ObjectBehavior;
+use PhpSpec\Wrapper\Subject;
+use PhpSpec\Wrapper\Subject\WrappedObject;
 use Prophecy\Argument;
 
 use PhpSpec\Wrapper\Subject\Expectation\ExpectationInterface;
@@ -14,10 +16,20 @@ class ConstructorDecoratorSpec extends ObjectBehavior
         $this->beConstructedWith($expectation);
     }
 
-    function it_is_initializable()
+    function it_rethrows_php_errors_as_phpspec_error_exceptions(Subject $subject, WrappedObject $wrapped)
     {
-        $this->shouldHaveType('PhpSpec\Wrapper\Subject\Expectation\ExpectationInterface');
+        // calling $subject->getWrappedObject() actually breaks Collaborator
+        // as there is a method with that name on it.
+        // One of the odds of testing a framework with itself
+        $subject->__call('getWrappedObject', array())->willThrow('PhpSpec\Exception\Example\ErrorException');
+        $this->shouldThrow('PhpSpec\Exception\Example\ErrorException')->duringMatch('be', $subject, array(), $wrapped);
     }
 
+    function it_ignores_any_other_exception(Subject $subject, WrappedObject $wrapped)
+    {
+        $subject->__call('getWrappedObject', array())->willThrow('\Exception');
+        $wrapped->getClassName()->willReturn('\ArrayObject');
+        $this->shouldNotThrow('\Exception')->duringMatch('be', $subject, array(), $wrapped);
+    }
 
 }
