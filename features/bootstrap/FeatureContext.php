ff04c579feb4bded261ec52d6a2082e80ae966b4
Jakub Zalas <jakub@zalas.pl>
2013-12-30 17:02:06 +0000

    Initialized the features folder.

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,13 @@
+<?php
+
+use Behat\Behat\Context\BehatContext;
+
+class FeatureContext extends BehatContext
+{
+    /**
+     * @param array $parameters
+     */
+    public function __construct(array $parameters)
+    {
+    }
+}
