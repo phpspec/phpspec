9ebe03cc58a012b651d3c69e4489887612195344
Alexandre D'Eschambeault <xel1045@gmail.com>
2014-03-16 10:09:05 -0400

    Add PSR-0 underscore support to the specification file. Refactor and more tests is still needed.

--- src/PhpSpec/Locator/PSR0/PSR0Locator.php
+++ src/PhpSpec/Locator/PSR0/PSR0Locator.php
@@ -260,6 +260,55 @@ class PSR0Locator implements ResourceLocatorInterface
         return $resources;
     }
 
+    private function findSpecClassname($path)
+    {
+        // Find namespace and class name
+        $class     = '';
+        $namespace = '';
+        $buffer    = '';
+        $i         = 0;
+        $fp        = fopen($path, 'r');
+
+        while (!$class) {
+            if (feof($fp)) {
+                break;
+            }
+
+            $buffer .= fread($fp, 512);
+            $tokens = token_get_all($buffer);
+
+            if (strpos($buffer, '{') === false) {
+                continue;
+            }
+
+            for (;$i<count($tokens);$i++) {
+                if ($tokens[$i][0] === T_NAMESPACE) {
+                    for ($j=$i+1;$j<count($tokens); $j++) {
+                        if ($tokens[$j][0] === T_STRING) {
+                             $namespace .= '\\'.$tokens[$j][1];
+                        } else if ($tokens[$j] === '{' || $tokens[$j] === ';') {
+                             break;
+                        }
+                    }
+                }
+
+                if ($tokens[$i][0] === T_CLASS) {
+                    for ($j=$i+1;$j<count($tokens);$j++) {
+                        if ($tokens[$j] === '{') {
+                            $class = $tokens[$i+2][1];
+                        }
+                    }
+                }
+            }
+        }
+
+        // Build the classname
+        $classname  = $namespace !== '' ? trim($namespace, '\\').'\\' : '';
+        $classname .= $class;
+
+        return $classname;
+    }
+
     /**
      * @param string $path
      *
@@ -267,11 +316,25 @@ class PSR0Locator implements ResourceLocatorInterface
      */
     private function createResourceFromSpecFile($path)
     {
-        // cut "Spec.php" from the end
-        $relative = substr($path, strlen($this->fullSpecPath), -4);
-        $relative = preg_replace('/Spec$/', '', $relative);
+        $classname = $this->findSpecClassname($path);
 
-        return new PSR0Resource(explode(DIRECTORY_SEPARATOR, $relative), $this);
+        // Remove spec namespace from the begining of the classname.
+        $specNamespace = trim($this->getSpecNamespace(), '\\').'\\';
+
+        if (0 !== strpos($classname, $specNamespace)) {
+            throw new RuntimeException(sprintf(
+                'Spec class must be in the base spec namespace `%s`.',
+                $this->getSpecNamespace()
+            ));
+        }
+
+        $classname = substr($classname, strlen($specNamespace));
+
+        // cut "Spec" from the end
+        $classname = preg_replace('/Spec$/', '', $classname);
+
+        // Create the resource
+        return new PSR0Resource(explode('\\', $classname), $this);
     }
 
     private function validatePsr0Classname($classname)
