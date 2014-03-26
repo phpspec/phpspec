70ea9d31dc6071dd0c03f3cf90c06946ff6812bd
KIUCHI Satoshinosuke <scholar@hayabusa-lab.jp>
2014-03-27 03:43:18 +0900

    fix bug in instantinate with some arguments
    There is a bug in instantinate with some arguments.
    
    Example:
    
    class SomeSpec extends ObjectBehavior
    {
        public function let()
        {
            $this->beConstructedWith('foo');
        }
    
        public function it_is_foo()
        {
            $this->isFoo()->shouldBe(true);
        }
    
        public function it_is_bar()
        {
            $this->beConstructedWith('bar');
            $this->isBar()->shouldBe(true);
        }
    }
    
    `it_is_bar` spec is failed in this case.
    Because, it is not done to reset the instance at `beConstructedWith`.

--- src/PhpSpec/Wrapper/Subject/WrappedObject.php
+++ src/PhpSpec/Wrapper/Subject/WrappedObject.php
@@ -80,6 +80,7 @@ class WrappedObject
         $this->classname      = $classname;
         $unwrapper            = new Unwrapper;
         $this->arguments      = $unwrapper->unwrapAll($arguments);
+        $this->instance       = null;
         $this->isInstantiated = false;
     }
 
