5541c70ce9e1de32a4d1f08eff84541c2339f7ad
Jeff Welch <whatthejeff@gmail.com>
2014-06-24 11:56:40 -0400

    [WIP] Fix instantiation for latest PHP versions.
    PHP 5.4.29 and 5.5.13 introduced a BC break that prevents using serialization
    as a means for instantiating internal objects without calling their
    constructor (unless they implement `Serializable`) [[1]]. This issue is still
    evolving and workarounds are currently being discussed in the PHP internals
    mailing list [[2]].
    
    In the mean time, since many PHP libraries rely on this functionality which
    is becoming increasingly complicated to support, @Ocramius has created
    [`ocramius/instantiator`](https://github.com/Ocramius/Instantiator) [[3]]. This
    commit adds support for `ocramius/instantiator` so that phpspec can
    continue to support this feature and stay in sync with other PHP libraries
    which support this feature without having to duplicate logic/updates across
    multiple projects.
    
    Please note that switching to `ocramius/instantiator` doesn't immediately solve
    all problems related to this BC break. Right now some of our tests are still
    failing. Ideally, as work arounds become available, they will be added to
    `ocramius/instantiator`.
    
    [1]:http://news.php.net/php.internals/74654
    [2]:http://marc.info/?t=140347878100001&r=1&w=2
    [3]:https://twitter.com/s_bergmann/status/475284594141691904

--- src/PhpSpec/Util/Instantiator.php
+++ src/PhpSpec/Util/Instantiator.php
@@ -31,80 +31,11 @@ class Instantiator
      */
     public function instantiate($className)
     {
-        return unserialize($this->createSerializedObject($className));
-    }
-
-    /**
-     * @param string $className
-     *
-     * @return string
-     *
-     * @throws \PhpSpec\Exception\Fracture\ClassNotFoundException
-     */
-    private function createSerializedObject($className)
-    {
         if (!class_exists($className)) {
             throw new ClassNotFoundException("Class $className does not exist.", $className);
         }
 
-        $reflection = new ReflectionClass($className);
-        $properties = $reflection->getProperties();
-
-        return "O:" . strlen($className) . ":\"$className\":" . count($properties) .
-            ':{' . $this->serializeProperties($reflection, $properties) ."}";
-    }
-
-    /**
-     * @param ReflectionClass      $reflection
-     * @param ReflectionProperty[] $properties
-     *
-     * @return string
-     */
-    private function serializeProperties(ReflectionClass $reflection, array $properties)
-    {
-        $serializedProperties = '';
-
-        foreach ($properties as $property) {
-            $serializedProperties .= $this->serializePropertyName($reflection, $property);
-            $serializedProperties .= $this->serializePropertyValue($reflection, $property);
-        }
-
-        return $serializedProperties;
-    }
-
-    /**
-     * @param ReflectionClass    $class
-     * @param ReflectionProperty $property
-     *
-     * @return string
-     */
-    private function serializePropertyName(ReflectionClass $class, ReflectionProperty $property)
-    {
-        $propertyName = $property->getName();
-
-        if ($property->isProtected()) {
-            $propertyName = chr(0) . '*' . chr(0) . $propertyName;
-        } elseif ($property->isPrivate()) {
-            $propertyName = chr(0) . $class->getName() . chr(0) . $propertyName;
-        }
-
-        return serialize($propertyName);
-    }
-
-    /**
-     * @param ReflectionClass    $class
-     * @param ReflectionProperty $property
-     *
-     * @return string
-     */
-    private function serializePropertyValue(ReflectionClass $class, ReflectionProperty $property)
-    {
-        $defaults = $class->getDefaultProperties();
-
-        if (array_key_exists($property->getName(), $defaults)) {
-            return serialize($defaults[$property->getName()]);
-        }
-
-        return serialize(null);
+        $instantiator = new \Instantiator\Instantiator();
+        return $instantiator->instantiate($className);
     }
 }
