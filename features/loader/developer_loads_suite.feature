6be246b362b1c575798b65c20ce486d89aae5d04
Tom Oram <tom@scl.co.uk>
2014-08-04 22:58:15 +0100

    Trigger an error when expected spec class is not found in file

---
+++ features/loader/developer_loads_suite.feature
@@ -0,0 +1,34 @@
+Feature: Developer loads suite
+  As a Developer
+  I want to load the test suite
+  In order to run my tests
+
+  @issue354
+  Scenario: Spec file does not contain expected class
+    Given the spec file "spec/Loader/SpecExample1/BadNameSpec.php" contains:
+    """
+    <?php
+
+    namespace spec\Loader\SpecExample1;
+
+    use PhpSpec\ObjectBehavior;
+    use Prophecy\Argument;
+
+    class IncorrectlyNamedSpec extends ObjectBehavior
+    {
+    }
+    """
+
+    And the class file "src/Loader/SpecExample1/BadName.php" contains:
+    """
+    <?php
+
+    namespace Loader\SpecExample1;
+
+    class BadName
+    {
+    }
+    """
+
+    When I run phpspec
+    Then I should see "spec\Loader\SpecExample1\BadNameSpec was not defined."
--- src/PhpSpec/Loader/ResourceLoader.php
+++ src/PhpSpec/Loader/ResourceLoader.php
@@ -13,8 +13,14 @@
 
 namespace PhpSpec\Loader;
 
+<<<<<<< HEAD
 use PhpSpec\Util\MethodAnalyser;
 use PhpSpec\Locator\ResourceManagerInterface;
+=======
+use PhpSpec\Locator\ResourceInterface;
+use PhpSpec\Locator\ResourceManager;
+
+>>>>>>> Trigger an error when expected spec class is not found in file
 use ReflectionClass;
 use ReflectionMethod;
 
@@ -48,48 +54,71 @@ class ResourceLoader
     {
         $suite = new Suite();
         foreach ($this->manager->locateResources($locator) as $resource) {
-            if (!class_exists($resource->getSpecClassname()) && is_file($resource->getSpecFilename())) {
-                require_once $resource->getSpecFilename();
-            }
-            if (!class_exists($resource->getSpecClassname())) {
-                continue;
-            }
-
-            $reflection = new ReflectionClass($resource->getSpecClassname());
-
-            if ($reflection->isAbstract()) {
-                continue;
-            }
-            if (!$reflection->implementsInterface('PhpSpec\SpecificationInterface')) {
-                continue;
-            }
-
-            $spec = new Node\SpecificationNode($resource->getSrcClassname(), $reflection, $resource);
-            foreach ($reflection->getMethods(ReflectionMethod::IS_PUBLIC) as $method) {
-                if (!preg_match('/^(it|its)[^a-zA-Z]/', $method->getName())) {
-                    continue;
-                }
-                if (null !== $line && !$this->lineIsInsideMethod($line, $method)) {
-                    continue;
-                }
-
-                $example = new Node\ExampleNode(str_replace('_', ' ', $method->getName()), $method);
-
-                if ($this->methodAnalyser->reflectionMethodIsEmpty($method)) {
-                    $example->markAsPending();
-                }
-
-                $spec->addExample($example);
-            }
-
-            $suite->addSpecification($spec);
+            $this->loadResouceIntoSuite($resource, $line, $suite);
         }
 
         return $suite;
     }
 
     /**
-     * @param int              $line
+     * @param ResourceInterface $resource
+     * @param integer|null      $line
+     * @param Suite             $suite
+     */
+    private function loadResouceIntoSuite(ResourceInterface $resource, $line, Suite $suite)
+    {
+        if (!class_exists($resource->getSpecClassname()) && is_file($resource->getSpecFilename())) {
+            require_once $resource->getSpecFilename();
+        }
+
+        if (!class_exists($resource->getSpecClassname())) {
+            return;
+        }
+
+        $reflection = new ReflectionClass($resource->getSpecClassname());
+
+        if ($reflection->isAbstract()) {
+            return;
+        }
+
+        if (!$reflection->implementsInterface('PhpSpec\SpecificationInterface')) {
+            return;
+        }
+
+        $spec = new Node\SpecificationNode($resource->getSrcClassname(), $reflection, $resource);
+        foreach ($reflection->getMethods(ReflectionMethod::IS_PUBLIC) as $method) {
+            $this->addExampleToSpecification($method, $line, $spec);
+        }
+
+        $suite->addSpecification($spec);
+    }
+
+    /**
+     * @param ReflectionMethod       $method
+     * @param integer|null           $line
+     * @param Node\SpecificationNode $spec
+     */
+    private function addExampleToSpecification(ReflectionMethod $method, $line, Node\SpecificationNode $spec)
+    {
+        if (!preg_match('/^(it|its)[^a-zA-Z]/', $method->getName())) {
+            return;
+        }
+
+        if (null !== $line && !$this->lineIsInsideMethod($line, $method)) {
+            return;
+        }
+
+        $example = new Node\ExampleNode(str_replace('_', ' ', $method->getName()), $method);
+
+        if ($this->methodIsEmpty($method)) {
+            $example->markAsPending();
+        }
+
+        $spec->addExample($example);
+    }
+
+    /**
+     * @param $line
      * @param ReflectionMethod $method
      *
      * @return bool
