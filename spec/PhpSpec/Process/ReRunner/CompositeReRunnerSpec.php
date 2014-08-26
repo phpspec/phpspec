1db9007768cebbb130c0ac462b16f3e6fea32d7c
Ciaran McNulty <cmcnulty@inviqa.com>
2014-08-26 17:18:08 +0100

    Missing examples for CompositeReRunner

--- spec/PhpSpec/Process/ReRunner/CompositeReRunnerSpec.php
+++ spec/PhpSpec/Process/ReRunner/CompositeReRunnerSpec.php
@@ -31,18 +31,45 @@ class CompositeReRunnerSpec extends ObjectBehavior
         $this->isSupported()->shouldReturn(false);
     }
 
-    function it_is_supported_if_any_child_is_supported(ReRunner $reRunner1)
+    function it_is_supported_if_an_early_child_is_supported(ReRunner $reRunner1, ReRunner $reRunner2)
     {
         $reRunner1->isSupported()->willReturn(true);
+        $reRunner2->isSupported()->willReturn(false);
 
         $this->isSupported()->shouldReturn(true);
     }
 
-    function it_invokes_the_first_supported_child_to_rerun_the_suite(ReRunner $reRunner1)
+    function it_is_supported_if_a_child_later_in_the_list_is_supported(ReRunner $reRunner1, ReRunner $reRunner2)
+    {
+        $reRunner1->isSupported()->willReturn(false);
+        $reRunner2->isSupported()->willReturn(true);
+
+        $this->isSupported()->shouldReturn(true);
+    }
+
+    function it_invokes_the_first_supported_child_to_rerun_the_suite_even_if_later_children_are_supported(ReRunner $reRunner1, ReRunner $reRunner2)
     {
         $reRunner1->isSupported()->willReturn(true);
+        $reRunner2->isSupported()->willReturn(true);
+
         $reRunner1->reRunSuite()->shouldBeCalled();
 
         $this->reRunSuite();
+
+        $reRunner1->reRunSuite()->shouldHaveBeenCalled();
+        $reRunner2->reRunSuite()->shouldNotHaveBeenCalled();
+    }
+
+    function it_skips_early_child_if_it_is_not_supported_and_invokes_runsuite_on_later_supported_child(ReRunner $reRunner1, ReRunner $reRunner2)
+    {
+        $reRunner1->isSupported()->willReturn(false);
+        $reRunner2->isSupported()->willReturn(true);
+
+        $reRunner2->reRunSuite()->willReturn();
+
+        $this->reRunSuite();
+
+        $reRunner1->reRunSuite()->shouldNotHaveBeenCalled();
+        $reRunner2->reRunSuite()->shouldHaveBeenCalled();
     }
 }
