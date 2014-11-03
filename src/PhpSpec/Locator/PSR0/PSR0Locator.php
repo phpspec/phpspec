9530fc10ce97349bd5976f5386891ba3c63d97f2
Richard Miller <richard.miller@limethinking.co.uk>
2014-11-03 18:45:42 +0000

    Remove unnecessary formatting change

--- src/PhpSpec/Locator/PSR0/PSR0Locator.php
+++ src/PhpSpec/Locator/PSR0/PSR0Locator.php
@@ -151,8 +151,8 @@ class PSR0Locator implements ResourceLocatorInterface
         }
 
         return 0 === strpos($path, $this->srcPath)
-        || 0 === strpos($path, $this->specPath)
-            ;
+            || 0 === strpos($path, $this->specPath)
+        ;
     }
 
     /**
