24bbfc4462b8eed901a8e1f88cc46bdd923b9e79
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2014-11-25 22:23:08 +0100

    reformat empty method examples
    * those methods were formatted to comply to coding standards
    * in commit 4530033cc640a71166352b3cb1dbcd0dc43ecb47 which misses
    * the point for them

--- spec/PhpSpec/Util/MethodAnalyserSpec.php
+++ spec/PhpSpec/Util/MethodAnalyserSpec.php
@@ -37,9 +37,7 @@ class MethodAnalyserSpec extends ObjectBehavior
 
 class ExampleObject
 {
-    public function emptyMethod()
-    {
-    }
+    public function emptyMethod() {}
 
     public function commentedMethod()
     {
@@ -61,16 +59,12 @@ class ExampleObject
         // another comment
     }
 
-    public function nonEmptyOneLineMethod()
-    {
-        return 'foo';
-    }
+    public function nonEmptyOneLineMethod() { return 'foo'; }
+
     public function nonEmptyOneLineMethod2()
-    {
-        return 'foo';
-    }
-    public function nonEmptyOneLineMethod3()
-    {
+    { return 'foo'; }
+
+    public function nonEmptyOneLineMethod3() {
         return 'foo';
     }
 }
