d64e395e27997f907b342bc509d83b431daebca0
Damian Crawford <dcrawford@inviqa.com>
2015-06-13 15:37:24 +0100

    Removed Count from Shutdown, only required for testing

--- spec/PhpSpec/Process/Shutdown/ShutdownSpec.php
+++ spec/PhpSpec/Process/Shutdown/ShutdownSpec.php
@@ -8,13 +8,6 @@ use Prophecy\Argument;
 
 class ShutdownSpec extends ObjectBehavior
 {
-    function it_returns_the_count_of_the_actions_registered(ShutdownActionInterface $action)
-    {
-        $this->count()->shouldReturn(0);
-        $this->registerAction($action);
-        $this->count()->shouldReturn(1);
-    }
-
     function it_runs_register_shutdown(ShutdownActionInterface $action)
     {
         $action->runAction()->shouldBeCalled();
