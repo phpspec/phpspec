7cf85e1f9acc2704835826e304a202eb6f70e48d
Damian Crawford <dcrawford@inviqa.com>
2015-06-13 15:38:25 +0100

    Made CurrentExampleListener final

--- src/PhpSpec/Listener/CurrentExampleListener.php
+++ src/PhpSpec/Listener/CurrentExampleListener.php
@@ -18,7 +18,7 @@ use PhpSpec\Event\SuiteEvent;
 use PhpSpec\Message\CurrentExample;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
-class CurrentExampleListener implements EventSubscriberInterface {
+final class CurrentExampleListener implements EventSubscriberInterface {
 
     /**
      * @var CurrentExample
