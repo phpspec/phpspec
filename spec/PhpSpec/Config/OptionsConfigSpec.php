d85cb089a53123514956efaa5efbfbadc08e63d8
Toby Griffiths <toby@cubicmushroom.co.uk>
2015-10-10 14:46:40 +0100

    Added new constructor argument & tests for ignorePending config

--- spec/PhpSpec/Config/OptionsConfigSpec.php
+++ spec/PhpSpec/Config/OptionsConfigSpec.php
@@ -2,6 +2,7 @@
 
 namespace spec\PhpSpec\Config;
 
+use PhpSpec\Config\OptionsConfig;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
@@ -9,42 +10,56 @@ class OptionsConfigSpec extends ObjectBehavior
 {
     function it_says_rerun_is_enabled_when_setting_is_true()
     {
-        $this->beConstructedWith(false, false, true, false, false);
+        $this->beConstructedWith(false, false, true, false, false, false);
 
         $this->isReRunEnabled()->shouldReturn(true);
     }
 
     function it_says_rerun_is_not_enabled_when_setting_is_false()
     {
-        $this->beConstructedWith(false, false, false, false, false);
+        $this->beConstructedWith(false, false, false, false, false, false);
 
         $this->isReRunEnabled()->shouldReturn(false);
     }
 
     function it_says_faking_is_enabled_when_setting_is_true()
     {
-        $this->beConstructedWith(false, false, false, true, false);
+        $this->beConstructedWith(false, false, false, true, false, false);
 
         $this->isFakingEnabled()->shouldReturn(true);
     }
 
     function it_says_faking_is_not_enabled_when_setting_is_false()
     {
-        $this->beConstructedWith(false, false, false, false, false);
+        $this->beConstructedWith(false, false, false, false, false, false);
 
         $this->isFakingEnabled()->shouldReturn(false);
     }
 
+    function it_says_ignore_pending_is_false_when_setting_is_false()
+    {
+        $this->beConstructedWith(false, false, false, false, false, false);
+
+        $this->isIgnorePendingEnabled()->shouldReturn(false);
+    }
+
+    function it_says_ignore_pending_is_true_when_setting_is_false()
+    {
+        $this->beConstructedWith(false, false, false, false, '/path/to/file', true);
+        /** @var OptionsConfig $this */
+        $this->isIgnorePendingEnabled()->shouldReturn(true);
+    }
+
     function it_says_bootstrap_path_is_false_when_setting_is_false()
     {
-        $this->beConstructedWith(false, false, false, false, false);
+        $this->beConstructedWith(false, false, false, false, false, false);
 
         $this->getBootstrapPath()->shouldReturn(false);
     }
 
     function it_returns_bootstrap_path_when_one_is_specified()
     {
-        $this->beConstructedWith(false, false, false, false, '/path/to/file');
+        $this->beConstructedWith(false, false, false, false, '/path/to/file', false);
 
         $this->getBootstrapPath()->shouldReturn('/path/to/file');
     }
