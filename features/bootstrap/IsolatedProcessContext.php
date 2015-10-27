1aaf389228d26f0289d6dcf7da3e014712477dbb
Elfiggo <fig@hotmail.co.uk>
2015-10-27 10:44:58 +0000

    HHVM Debug tracing

--- features/bootstrap/IsolatedProcessContext.php
+++ features/bootstrap/IsolatedProcessContext.php
@@ -114,6 +114,8 @@ class IsolatedProcessContext implements Context, SnippetAcceptingContext
      */
     public function iShouldSee($message)
     {
+        var_dump($this->lastOutput);
+        var_dump($message);
         expect(strpos($this->lastOutput, $message))->toNotBe(false);
     }
 
