25911d02c22490ebccc72e57f9f18f03885cd542
Chris Emerson <chris.emerson@bunches.co.uk>
2015-12-03 12:15:24 +0000

    Create a new WindowsPassthruRerunner that fixes the command line on windows & ensure it gets used on windows instead of the standard PassthruRerunner. Fixes #773

---
+++ spec/PhpSpec/Process/ReRunner/WindowsPassthruRerunnerSpec.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of PhpSpec, A php toolset to drive emergent
+ * design by specification.
+ *
+ * (c) Marcello Duarte <marcello.duarte@gmail.com>
+ * (c) Konstantin Kudryashov <ever.zet@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace spec\PhpSpec\Process\ReRunner;
+
+use PhpSpec\ObjectBehavior;
+use PhpSpec\Process\Context\ExecutionContextInterface;
+use Prophecy\Argument;
+use Symfony\Component\Process\PhpExecutableFinder;
+
+class WindowsPassthruRerunnerSpec extends ObjectBehavior
+{
+    function let(PhpExecutableFinder $executableFinder, ExecutionContextInterface $executionContext)
+    {
+        $this->beConstructedThrough('withExecutionContext', array($executableFinder, $executionContext));
+    }
+
+    function it_is_a_rerunner()
+    {
+        $this->shouldHaveType('PhpSpec\Process\ReRunner');
+    }
+
+    function it_is_not_supported_when_php_process_is_not_found(PhpExecutableFinder $executableFinder)
+    {
+        $executableFinder->find()->willReturn(false);
+
+        $this->isSupported()->shouldReturn(false);
+    }
+}
