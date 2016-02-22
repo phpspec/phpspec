d81a4c3fdf3ec3a7819a7b26868109cfe41133f7
Marek Pietrzak <mpietrzak@inviqa.com>
2016-02-22 10:04:30 +0000

    Remove optional dependency of NameChecker

--- src/PhpSpec/Listener/MethodNotFoundListener.php
+++ src/PhpSpec/Listener/MethodNotFoundListener.php
@@ -45,12 +45,12 @@ class MethodNotFoundListener implements EventSubscriberInterface
         IO $io,
         ResourceManagerInterface $resources,
         GeneratorManager $generator,
-        NameCheckerInterface $nameChecker = null
+        NameCheckerInterface $nameChecker
     ) {
         $this->io        = $io;
         $this->resources = $resources;
         $this->generator = $generator;
-        $this->nameChecker = $nameChecker ?: new ReservedWordsMethodNameChecker();
+        $this->nameChecker = $nameChecker;
     }
 
     public static function getSubscribedEvents()
