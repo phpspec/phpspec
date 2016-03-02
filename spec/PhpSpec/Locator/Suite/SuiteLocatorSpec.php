948e7cf884bc42331f4987fc28d6d0be98b09e98
Alexandre Lemaire <alemaire@circlical.com>
2016-03-02 12:33:49 -0500

    Replacing tabs with spaces.

--- spec/PhpSpec/Locator/Suite/SuiteLocatorSpec.php
+++ spec/PhpSpec/Locator/Suite/SuiteLocatorSpec.php
@@ -12,34 +12,34 @@ class SuiteLocatorSpec extends ObjectBehavior
     private $srcPath;
     private $specPath;
 
-    function let( Filesystem $fs )
+    function let(Filesystem $fs)
     {
-        $this->srcPath  = realpath( __DIR__ . '/../../../../src' );
-        $this->specPath = realpath( __DIR__ . '/../../../../' );
+        $this->srcPath = realpath(__DIR__ . '/../../../../src');
+        $this->specPath = realpath(__DIR__ . '/../../../../');
     }
 
     function it_is_a_locator()
     {
-        $this->shouldBeAnInstanceOf( 'PhpSpec\Locator\ResourceLocatorInterface' );
+        $this->shouldBeAnInstanceOf('PhpSpec\Locator\ResourceLocatorInterface');
     }
 
     function its_priority_is_zero()
     {
-        $this->getPriority()->shouldReturn( 0 );
+        $this->getPriority()->shouldReturn(0);
     }
 
 
-    function it_supports_queries_that_use_suite_name( Filesystem $fs, SplFileInfo $file )
+    function it_supports_queries_that_use_suite_name(Filesystem $fs, SplFileInfo $file)
     {
-        $this->beConstructedWith( 'SuiteName', '', 'spec', dirname( __DIR__ ), __DIR__, $fs );
+        $this->beConstructedWith('SuiteName', '', 'spec', dirname(__DIR__), __DIR__, $fs);
 
-        $this->supportsQuery( 'SuiteName' )->shouldBe( true );
+        $this->supportsQuery('SuiteName')->shouldBe(true);
     }
 
-    function it_returns_an_empty_array_when_suite_name_does_not_exist( Filesystem $fs, SplFileInfo $file )
+    function it_returns_an_empty_array_when_suite_name_does_not_exist(Filesystem $fs, SplFileInfo $file)
     {
-        $this->beConstructedWith( 'SuiteName', '', 'spec', dirname( __DIR__ ), __DIR__, $fs );
+        $this->beConstructedWith('SuiteName', '', 'spec', dirname(__DIR__), __DIR__, $fs);
 
-        $this->supportsQuery( 'OtherName' )->shouldBe( false );
+        $this->supportsQuery('OtherName')->shouldBe(false);
     }
 }
