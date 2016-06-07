76f24aa84a1235ce946d21545f6d7a777553872c
Sam-Burns <sam.burns.uk@gmail.com>
2016-06-07 18:28:57 +0100

    Removing class property

--- src/PhpSpec/Container/ContainerBuilder.php
+++ src/PhpSpec/Container/ContainerBuilder.php
@@ -8,22 +8,20 @@ use PhpSpec\Container\ServiceContainer;
 class ContainerBuilder
 {
     /**
-     * @var CompositeContainer
+     * @return CompositeContainer
      */
-    private $compositeContainer;
-
     public function buildContainer()
     {
-        $this->compositeContainer = new CompositeContainer();
+        $compositeContainer = new CompositeContainer();
 
         $container = new ServiceContainer();
-        $this->compositeContainer->setPhpSpecContainer($container);
+        $compositeContainer->setPhpSpecContainer($container);
         $containerConfig = new ServiceProvider();
         $containerConfig->build($container);
 
-        $container->setCompositeContainer($this->compositeContainer);
-        $this->compositeContainer->addContainer($container);
+        $container->setCompositeContainer($compositeContainer);
+        $compositeContainer->addContainer($container);
 
-        return $this->compositeContainer;
+        return $compositeContainer;
     }
 }
