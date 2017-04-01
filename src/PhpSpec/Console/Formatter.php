e135ddc97d0f066816f00a3cdb470c2cdf577d46
aa <andrey.astakhov@gmail.com>
2017-04-01 23:27:42 +0200

    Removed black background of line numbers for 'pretty' format.

--- src/PhpSpec/Console/Formatter.php
+++ src/PhpSpec/Console/Formatter.php
@@ -47,7 +47,7 @@ class Formatter extends OutputFormatter
         $this->setStyle('passed-bg', new OutputFormatterStyle('black', 'green', array('bold')));
 
         $this->setStyle('value', new OutputFormatterStyle('yellow'));
-        $this->setStyle('lineno', new OutputFormatterStyle(null, 'black'));
+        $this->setStyle('lineno', new OutputFormatterStyle(null));
         $this->setStyle('code', new OutputFormatterStyle('white'));
         $this->setStyle('label', new OutputFormatterStyle('white', null, array('bold')));
         $this->setStyle('hl', new OutputFormatterStyle('black', 'yellow', array('bold')));
