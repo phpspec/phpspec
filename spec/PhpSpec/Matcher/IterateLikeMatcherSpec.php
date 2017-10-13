2cd0619583b3dae31953562c56c438bda151de92
Samuel ROZE <samuel.roze@gmail.com>
2017-10-13 11:25:15 +0100

    Uses `var_exports` to present the value

--- spec/PhpSpec/Matcher/IterateLikeMatcherSpec.php
+++ spec/PhpSpec/Matcher/IterateLikeMatcherSpec.php
@@ -68,7 +68,11 @@ final class IterateLikeMatcherSpec extends ObjectBehavior
         $second->foo = 'bar';
 
         $this
-            ->shouldThrow(new SubjectElementDoesNotMatchException(1, '"c"', '"d"', '"c"', '"e"'))
+            ->shouldThrow(new SubjectElementDoesNotMatchException(0, '"0"', '"stdClass::__set_state(array(
+   \'foo\' => \'foo\',
+))"', '"0"', '"stdClass::__set_state(array(
+   \'foo\' => \'bar\',
+))"'))
             ->during('positiveMatch', [
                 'iterateLike',
                 $this->createGeneratorReturningArray([$first]),
