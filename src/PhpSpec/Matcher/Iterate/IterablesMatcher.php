844bd093d497e6221faf6e33178aeb0a158e5ad5
Ciaran McNulty <mail@ciaranmcnulty.com>
2017-10-15 05:57:06 +0100

    Restore previous strictness

--- src/PhpSpec/Matcher/Iterate/IterablesMatcher.php
+++ src/PhpSpec/Matcher/Iterate/IterablesMatcher.php
@@ -29,7 +29,7 @@ final class IterablesMatcher
      * @throws SubjectHasFewerElementsException
      * @throws SubjectHasMoreElementsException
      */
-    public function match($subject, $expected, bool $strict = false)
+    public function match($subject, $expected, bool $strict = true)
     {
         if (!$this->isIterable($subject)) {
             throw new \InvalidArgumentException('Subject value should be an array or implement \Traversable.');
