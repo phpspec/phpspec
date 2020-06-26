bb722393fab9f1d71f610ef75938299f7ea50983
Kuzia <kuzia@kuzia.space>
2020-06-26 17:58:49 +0300

    check for symfony eventDispatcher contract on BaseEvent

--- src/PhpSpec/Event/BaseEvent.php
+++ src/PhpSpec/Event/BaseEvent.php
@@ -2,10 +2,12 @@
 
 namespace PhpSpec\Event;
 
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\EventDispatcher\Event as OldEvent;
 use Symfony\Contracts\EventDispatcher\Event as ContractEvent;
 
-if (class_exists(ContractEvent::class)) {
+if (\is_subclass_of(EventDispatcher::class, EventDispatcherInterface::class)) {
     class BaseEvent extends ContractEvent
     {
     }
